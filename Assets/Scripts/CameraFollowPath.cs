c74bf926a34b97560f57659b9ed77efcc260c8e6
briansenas <briansena23@hotmail.com>
2023-05-23 23:23:49 +0200

    Beta: Non-Lineal Camera Movement

--- Assets/Scripts/CameraFollowPath.cs
+++ Assets/Scripts/CameraFollowPath.cs
@@ -1,15 +1,18 @@
 using UnityEngine;
+using TMPro;
 using System.Collections;
 
 public class CameraFollowPath : MonoBehaviour {
 
-	public float speed = 3f;
-    public float waitFor = 5f; 
-    public float maxSpeed = 10f; 
+  [SerializeField] private AnimationCurve _animationCurve; 
+  [SerializeField] private TextMeshProUGUI TimerText;
+
+	public float speed = 0f;
+  public float waitFor = 5f; 
 	public Transform pathParent;
 	Transform targetPoint;
 	int index;
-
+  
 	void OnDrawGizmos()
 	{
 		Vector3 from;
@@ -28,20 +31,35 @@ public class CameraFollowPath : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
         if(Time.time > waitFor)
         {
-            transform.position = Vector3.MoveTowards(transform.position, targetPoint.position, speed * Time.deltaTime);
+            disableTimer(); 
+            transform.position = Vector3.MoveTowards(transform.position, targetPoint.position, _animationCurve.Evaluate(speed));
             if (Vector3.Distance(transform.position, targetPoint.position) < 0.1f) 
             {
                 index++;
                 index %= pathParent.childCount;
                 targetPoint = pathParent.GetChild (index);
                 speed++; 
-                if(speed>=maxSpeed){
-                    speed = maxSpeed;
-                }
             }
         }
+        else{
+          updateTimer(waitFor - Time.time); 
+        }
+
 	}
+
+  public void disableTimer(){
+    TimerText.text = "Run! You must to survive."; 
+  }
+
+  public void updateTimer(float time_){
+      if (TimerText) TimerText.enabled=true;
+      time_ += 1;
+      float minutes = Mathf.FloorToInt(time_ / 60); 
+      float seconds = Mathf.FloorToInt(time_ % 60); 
+
+      TimerText.text = string.Format("Steady: {00}", seconds);
+  }
 }
