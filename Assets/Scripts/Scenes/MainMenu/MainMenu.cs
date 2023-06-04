34ccf3dd1f5a3d6aefdce8ae6c2715af8cec7853
Javi Vélez <javichuvj8@gmail.com>
2023-06-04 16:58:34 +0200

    Settings menu completed

--- Assets/Scripts/Scenes/MainMenu/MainMenu.cs
+++ Assets/Scripts/Scenes/MainMenu/MainMenu.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement; 
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
@@ -25,5 +27,38 @@ public class MainMenu : MonoBehaviour
 
     public void QuitGame(){
         Application.Quit(); 
+    }
+
+    //SETTINGS MENU
+    public AudioMixer mainMixer;
+    public float defaultVolume;
+    public Slider volumeSlider;
+    public Toggle fullscreenToggle;
+
+    void Start()
+    {
+        if(volumeSlider){
+            volumeSlider.value = PlayerPrefs.GetFloat("volume");
+        }    
+        
+        if(fullscreenToggle){
+            fullscreenToggle.isOn = Screen.fullScreen;
+        }     
+    }
+
+    void Update()
+    {
+        mainMixer.SetFloat("volume", defaultVolume);
+        PlayerPrefs.SetFloat("volume", defaultVolume);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen; 
+    }
+
+    public void SetVolume(float volume)
+    {
+        defaultVolume = volume;
     }
 }
